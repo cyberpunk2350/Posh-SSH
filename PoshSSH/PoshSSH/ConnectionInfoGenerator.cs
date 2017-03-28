77d67dbdb989441715d6a8bcecac2e29b50470b4
Carlos Perez <carlos_perez@darkoperator.com>
2017-03-27 20:38:57 -0400

    first crack at proxy fix

--- PoshSSH/PoshSSH/ConnectionInfoGenerator.cs
+++ PoshSSH/PoshSSH/ConnectionInfoGenerator.cs
@@ -159,7 +159,7 @@ namespace SSH
                         break;
                 }
 
-                if (proxycredential.UserName != String.Empty)
+                if (proxycredential == null)
                 {
                     connectionInfo = new ConnectionInfo(computer,
                                                         port,
